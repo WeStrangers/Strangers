27b3050226ab31488b049516e22ad2070c2db804
Alexander <s555b@mail.ru>
2024-04-18 15:15:34 +0300

    Product page

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,13 @@
 <template>
-  <div class="w-screen min-h-screen dark:text-white dark:bg-slate-950">
-		<Navigation class="w-48" />
-		<div class="ms-48">
-			<main class="min-h-screen" >
-				<router-view v-slot="{ Component }">
-					<!-- <KeepAlive> -->
-						<transition mode="out-in">
-							<component class="p-4" :is="Component" />
-						</transition>
-					<!-- </KeepAlive> -->
-				</router-view>
-			</main>
-			<Footer />
-		</div>
+  <div class="w-screen min-h-screen flex dark:text-white dark:bg-slate-950">
+		<h1 class="text-4xl m-auto">Coming Soon</h1>
 	</div>
 </template>
 
 <script setup lang="ts">
-import Navigation from './components/root/Navigation.vue';
-import Footer from './components/root/Footer.vue';
 
-// Whenever the user explicitly chooses light mode
-// localStorage.theme = 'light'
-
-// Whenever the user explicitly chooses dark mode
-// localStorage.theme = 'dark'
-
-// Whenever the user explicitly chooses to respect the OS preference
-// localStorage.removeItem('theme')
 </script>
 
 <style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.25s ease;
-}
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
 </style>
