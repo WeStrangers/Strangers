e2971ac2c5898fa465742cf155742f820beaf074
Alexander <s555b@mail.ru>
2024-05-22 23:44:59 +0300

    Add a GitHub repository

--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -2,6 +2,7 @@
   <footer class="flex flex-col justify-around h-auto px-4 py-6 text-slate-500 bg-slate-900">
 		<div class="flex space-x-2">
 			<p>{{ $t('footer.socials.text') }}</p>
+			<a href="https://github.com/WeStrangers/Strangers" target="_blank">{{ $t('footer.socials.links.github')}}</a>
 			<a href="https://poti.netlify.app" target="_blank">{{ $t('footer.socials.links.developer')}}</a>
 		</div>
 		<div class="flex flex-col *:text-center">
